831a1e75c16b30acb009ccbb8f7a4ba2061875be
marcelo2932 <marcelohenriquezrenca@gmail.com>
2024-03-07 20:19:29 -0300

    viajes Chile

---
+++ assets/js/scrips.js
@@ -0,0 +1,12 @@
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
